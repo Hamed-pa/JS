d4d3786fb18eafa5d176f1cb94bea6366379e7c7
Hamed Qaderi <hamedghaderii@gmail.com>
2018-12-13 13:38:06 +0330

    Debugged

--- test/test.js
+++ test/test.js
@@ -2,16 +2,34 @@ const chai = require("chai");
 let assert = chai.assert;
 
 describe("chaining", function () {
+    let aAccumulator = new Accumulator(5);
+
+    it('initializes the accumulator', function() {
+        assert.equal(aAccumulator.number, 5);
+    });
+
     it('sum a previous number with 10', function () {
-        assert.equal(aAccumulator.read(10), 15);
+        aAccumulator.read(10);
+
+        assert.equal(aAccumulator.valu(), 15);
     });
 
     it("again sum a previous result with 11", function () {
-        assert.equal(aAccumulator.read(11), 26);
+        aAccumulator.read(11);
+
+        assert.equal(aAccumulator.valu(), 26);
     });
 
-    it("shaow result", function () {
+    it("again sum a previous result with 0", function () {
+        aAccumulator.read(0);
+
         assert.equal(aAccumulator.valu(), 26);
+    });
+
+    it("again sum a previous result with -12", function () {
+        aAccumulator.read(-12);
+
+        assert.equal(aAccumulator.valu(), 14);
     });
 });
 
@@ -22,13 +40,13 @@ function Accumulator(num) {
 
     this.read = function (num) {
         this.number += num;
-        return this.number;
+        // return this.number;  it doesn't need to return a value
     };
     this.valu = function () {
         return this.number;
     };
 };
 
-let aAccumulator = new Accumulator(5);
+
 
 
