58468c25a546b3346913eb9050deee540ad59191
Hamed Pa <hamedpa6@gmail.com>
2019-01-03 12:08:08 +0330

    session 14 t5

--- src/main.js
+++ src/main.js
@@ -1,22 +1,23 @@
 'use strict';
 
-let array = [6, 17, 5, 4, 1, 10, 12];
+let array = ["HTML", "JavaScript", "CSS", "HTTP"];
 
-function sortReverse(array){
+function copySorted(arr){
     let x = 0 ;
 
     for (let j = 0 ; j < (array.length) ; j++){
 
         for (let i = 0 ; i < array.length ; i++) {
 
-            if (array[i] < array[i+1]) {
+            if (array[i] > array[i+1]) {
                 x = array[i];
                 array[i] = array[i+1];
                 array[i+1] = x;
             };
         };
     };
+    return arr;
 };
 
-sortReverse(array);
-console.log(array);
+console.log(copySorted(array));
+
--- test/test.js
+++ test/test.js
@@ -1,28 +1,29 @@
 const chai = require("chai");
 let assert = chai.assert;
 
-describe("filter range", function () {
-    it('Between numbers 10 and 20', function () {
-        assert.equal(sortReverse(array).join(), [17, 12, 10, 6, 5, 4, 1]);
+describe("copy sorted", function () {
+    it('string sort', function () {
+        assert.equal(copySorted(array).join(), ["CSS", "HTML", "HTTP", "JavaScript"]);
     });
 
 
 });
-let array = [6, 17, 5, 4, 1, 10, 12];
 
-function sortReverse(array) {
+let array = ["HTML", "JavaScript", "CSS", "HTTP"];
+
+function copySorted(arr) {
     let x = 0;
 
-    for (let j = 0; j < (array.length ); j++) {
+    for (let j = 0; j < (array.length); j++) {
 
         for (let i = 0; i < array.length; i++) {
 
-            if (array[i] < array[i + 1]) {
+            if (array[i] > array[i + 1]) {
                 x = array[i];
                 array[i] = array[i + 1];
                 array[i + 1] = x;
             };
         };
     };
-    return array;
+    return arr;
 };
