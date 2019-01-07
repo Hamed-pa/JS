59ab443fda214e9307206e929e3146eb62e3d813
Hamed Pa <hamedpa6@gmail.com>
2019-01-07 16:32:27 +0330

    Second

--- src/main.js
+++ src/main.js
@@ -1,64 +1,12 @@
 'use strict';
 
-function validBraces(str) {
-    let result = 0;
+function rightTriangle(a, b, c) {
 
-    if (str.includes("{")){
-
-       if (str.includes("}")){
-           result++;
-       } else {
-           result--;
-       };
-    };
-
-    if (str.includes("}")) {
-        if (str.includes("{")) {
-            result++;
-        } else {
-            result--;
-        };
-    };
-
-    if (str.includes("[")){
-        if (str.includes("]")) {
-            result++;
-        } else {
-            result--;
-        }
-    };
-
-    if (str.includes("]")) {
-
-        if (str.includes("[")) {
-            result++;
-        } else {
-            result--;
-        };
-    };
-
-    if (str.includes("(")) {
-        if (str.includes(")")) {
-            result++;
-        } else {
-            result--;
-        }
-    }
-
-    if (str.includes(")")) {
-        if (str.includes("(")) {
-            result++;
-        } else {
-            result--;
-        };
-    };
-
-    if (result > 0 && result % 2 == 0){
-        return true;
-    } else {
+    if (a + c < b || b + c < a || a + b < c) {
         return false;
     }
+
+    return true;
 };
 
-
-console.log (validBraces("[}"));
+console.log(rightTriangle(7, 2, 2));
--- test/test.js
+++ test/test.js
@@ -1,80 +1,28 @@
 const chai = require("chai");
 let assert = chai.assert;
 
-describe("check valid braces", function () {
-    it('check valid braces', function () {
-        assert.isTrue(validBraces("(){}[]"));
+describe("Right Triangle", function () {
+    it('Right Triangle', function () {
+        assert.isTrue(rightTriangle(1, 2, 2));
     });
 
-    it('check valid braces', function () {
-        assert.isTrue(validBraces("([{}])"));
+    it('Right Triangle', function () {
+        assert.isFalse(rightTriangle(7, 2, 2));
     });
 
-    it('check valid braces', function () {
-        assert.isFalse(validBraces("[}"));
+    it('Right Triangle', function () {
+        assert.isTrue(rightTriangle(3, 4, 3));
     });
 
 
 });
-function validBraces(str) {
-    let result = 0;
 
-    if (str.includes("{")) {
+function rightTriangle(a, b, c) {
 
-        if (str.includes("}")) {
-            result++;
-        } else {
-            result--;
-        };
-    };
-
-    if (str.includes("}")) {
-        if (str.includes("{")) {
-            result++;
-        } else {
-            result--;
-        };
-    };
-
-    if (str.includes("[")) {
-        if (str.includes("]")) {
-            result++;
-        } else {
-            result--;
-        }
-    };
-
-    if (str.includes("]")) {
-
-        if (str.includes("[")) {
-            result++;
-        } else {
-            result--;
-        };
-    };
-
-    if (str.includes("(")) {
-        if (str.includes(")")) {
-            result++;
-        } else {
-            result--;
-        }
-    }
-
-    if (str.includes(")")) {
-        if (str.includes("(")) {
-            result++;
-        } else {
-            result--;
-        };
-    };
-
-    if (result > 0 && result % 2 == 0) {
-        return true;
-    } else {
+    if (a + c < b || b + c < a || a + b < c) {
         return false;
     }
+
+    return true;
 };
-
-
 
